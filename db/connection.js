052f7e8d89ba7a7693feb932ba85d7abb987c216
ezzADJG <josephdoloriert14@gmail.com>
2025-07-15 18:57:42 -0500

    ajuste de la base de datos

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'aldair',
-  password: 'admin',
-  database: 'turismo_web',
+  host: 'sql.freedb.tech',
+  user: 'freedb_aldair',
+  password: '!8w$4tqsJ8Ut?EF',
+  database: 'freedb_turismo_web',
   waitForConnections: true,
   connectionLimit: 10,
 });

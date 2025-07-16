309860132b14e3dbce140308e68e1e0c9153784c
ezzADJG <josephdoloriert14@gmail.com>
2025-07-15 20:23:14 -0500

    Ajustes

--- src/server.js
+++ src/server.js
@@ -25,13 +25,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.get('/test-api', (req, res) => {
-  res.sendFile(path.join(__dirname, '../test-api.html'));
-});
-
-app.get('/test-resenas', (req, res) => {
-  res.sendFile(path.join(__dirname, '../test-resenas.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
